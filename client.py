654b9a2c349e1ebc6896eb8b5db7163e0bd240b9
Timur <timtuka8@gmail.com>
2023-11-03 21:28:06 +0500

    ver 1.0

--- client.py
+++ client.py
@@ -1,35 +1,58 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
-from twisted.internet.protocol import ClientFactory as ClFactory
+from twisted.internet.protocol import ReconnectingClientFactory as ClFactory
 from twisted.internet.endpoints import TCP4ClientEndpoint
+from sys import stderr
+import json
 
 
 class Client(Protocol):
     def __init__(self):
-        reactor.callInThread(self.send_data)
+        reactor.callInThread(self.message_input)
+
+    @staticmethod
+    def __encode_json(**kwargs):
+        return json.dumps(kwargs)
 
     def connectionMade(self):
-        print("Write your name")
+        pass
 
     def dataReceived(self, data):
-        print(data.decode("utf-8"))
+        try:
+            data = json.loads(data.decode("utf-8"))
+        except UnicodeDecodeError or json.JSONDecodeError:
+            print("Something went wrong", file=stderr)
+            return
+        if data["type"] == "error":
+            print(data.get("value", "Unknown error"), file=stderr)
+        else:
+            print(data.get("value", "no value in the message"))
 
-    def send_data(self):
+    def send_data(self, **kwargs):
+        self.transport.write(self.__encode_json(**kwargs).encode("utf-8"))
+
+    def message_input(self):
         while True:
-            self.transport.write(input().encode("utf-8"))
+            self.send_data(value=input("value: "), type=input("type: "))
 
 
 class ClientFactory(ClFactory):
     def buildProtocol(self, addr):
         return Client()
 
+    # def clientConnectionFailed(self, connector, reason):
+    #     print(reason)
+    #     ClFactory.clientConnectionFailed(self, connector, reason)
+
+    # def clientConnectionLost(self, connector, reason):
+    #     print(reason)
+    #     ClFactory.clientConnectionLost(self, connector, reason)
+    def clientConnectionLost(self, connector, unused_reason):
+        self.retry(connector)
+
     def clientConnectionFailed(self, connector, reason):
         print(reason)
-        ClFactory.clientConnectionFailed(self, connector, reason)
-
-    def clientConnectionLost(self, connector, reason):
-        print(reason)
-        ClFactory.clientConnectionLost(self, connector, reason)
+        self.retry(connector)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -3,49 +3,96 @@ from twisted.internet.protocol import Protocol, connectionDone
 from twisted.internet.protocol import ServerFactory as ServFactory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.python import failure
+import json
 
 
 class Server(Protocol):
-    def __init__(self, clients):
+    def __init__(self, clients, my_id):
         self.clients = clients
-        self.name = ""
+        self.another_client = None
+        self.my_id = my_id
 
     def connectionMade(self):
-        print("New connection")
+        self.clients[self.my_id] = self
+        print(self.clients)
 
-    def add_user(self, name):
-        if name not in self.clients:
-            self.clients[self] = name
-            self.name = name
-        else:
-            self.transport.write("Wrong username, try another".encode("utf-8"))
+    # def add_user(self, name):
+    #     if name not in self.clients:
+    #         self.clients[self.my_id] = self
 
-    def send_message(self, data, receiver=None):
-        if receiver:
-            receiver.transport.write(f"{self.name}: {data}".encode("utf-8"))
+    @staticmethod
+    def __encode_json(**kwargs):
+        return json.dumps(kwargs)
+
+    def send_message(self, **kwargs):
+        if kwargs.get("receiver"):
+            receiver = kwargs["receiver"]
+            del kwargs["receiver"]
+            receiver.transport.write(self.__encode_json(**kwargs).encode("utf-8"))
         else:
-            self.transport.write(data.encode("utf-8"))
+            self.transport.write(self.__encode_json(**kwargs).encode("utf-8"))
+        # if receiver:
+        #     receiver.transport.write(data.encode("utf-8"))
+        # else:
+        #     self.transport.write(data.encode("utf-8"))
 
     def dataReceived(self, data):
-        data = data.decode("utf-8")
-        if not self.name:
-            self.add_user(data)
+        try:
+            data = json.loads(data.decode("utf-8"))
+        except UnicodeDecodeError:
+            self.send_message(value="Cannot decode, use utf-8", type="error")
+            return
+        except json.JSONDecodeError:
+            self.send_message(value="Cannot decode, use json", type="error")
+            return
+        # if not self.name:
+        #     self.add_user(data)
+        #     return
+        if not data.get("type") or not data.get("value"):
+            self.send_message(value="Wrong data", type="error")
             return
 
-        for protocol in self.clients.keys():
-            if protocol != self:
-                self.send_message(data, protocol)
+        if data["type"] == "user_choose":
+            try:
+                another_client = int(data["value"])
+                if another_client in self.clients.keys():
+                    self.another_client = another_client
+                else:
+                    raise KeyError
+            except KeyError:
+                self.send_message(value="Can't find that client", type="error")
+            except ValueError:
+                self.send_message(value="Write another id as int", type="error")
+            else:
+                self.send_message(value=f"Talk to {self.another_client}", type="user_chosen")
+        elif data["type"] == "new_message":
+            if not self.another_client:
+                self.send_message(value="Try another client", type="error")
+            try:
+                self.send_message(value=data["value"], receiver=self.clients[self.another_client], type="new_message")
+            except KeyError:
+                self.send_message(value="Try another client", type="error")
+                self.another_client = None
+
+        # for protocol in self.clients.keys():
+        #     if protocol != self:
+        #         self.send_message(data, protocol)
+
+    def disconnect(self):
+        del self.clients[self.my_id]
 
     def connectionLost(self, reason: failure.Failure = connectionDone):
-        del self.clients[self]
+        self.disconnect()
 
 
 class ServerFactory(ServFactory):
     def __init__(self):
         self.clients = {}
+        self.last_id = 0
 
     def buildProtocol(self, addr):
-        return Server(self.clients)
+        self.last_id += 1
+        return Server(self.clients, self.last_id)
 
 
 if __name__ == '__main__':
